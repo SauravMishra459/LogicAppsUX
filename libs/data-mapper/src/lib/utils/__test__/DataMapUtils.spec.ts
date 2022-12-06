78aa07a1c2c4e0e140a2088cf0bc9466c16602b6
Reid Fortier <refortie@microsoft.com>
2022-12-06 10:51:54 -0800

    WiP

--- libs/data-mapper/src/lib/utils/__test__/DataMapUtils.spec.ts
+++ libs/data-mapper/src/lib/utils/__test__/DataMapUtils.spec.ts
@@ -203,6 +203,43 @@ describe('utils/DataMap', () => {
       expect(resultEntries[3][1]).toBeTruthy();
     });
 
+    it('creates an index loop connection', () => {
+      simpleMap['ns0:Root'] = {
+        LoopingWithIndex: {
+          WeatherSummary: {
+            '$for(/ns0:Root/LoopingWithIndex/WeatherReport, $a)': {
+              Day: {
+                Name: 'concat("Day ", $a)',
+              },
+            },
+          },
+        },
+      };
+
+      const result = convertFromMapDefinition(simpleMap, extendedSource, extendedTarget, functionMock);
+      const resultEntries = Object.entries(result);
+      resultEntries.sort();
+
+      expect(resultEntries.length).toEqual(6);
+      /*
+      const concatId = resultEntries[0][0];
+
+  expect(resultEntries[0][0]).toEqual(concatId);
+  expect(resultEntries[0][1]).toBeTruthy();
+  expect(resultEntries[0][1].inputs[0][0]).toEqual('"Day "');
+  expect((resultEntries[0][1].inputs[0][1] as ConnectionUnit).reactFlowKey).toEqual('source-/ns0:Root/DirectTranslation/EmployeeName');
+  expect(resultEntries[0][1].outputs[0].reactFlowKey).toEqual('target-/ns0:Root/LoopingWithIndex/WeatherSummary/Day/Name');
+
+  expect(resultEntries[1][0]).toContain('source-/ns0:Root/DirectTranslation/EmployeeName');
+  expect(resultEntries[1][1]).toBeTruthy();
+  expect(resultEntries[1][1].outputs[0].reactFlowKey).toEqual(concatId);
+
+  expect(resultEntries[2][0]).toContain('target-/ns0:Root/DirectTranslation/Employee/Name');
+  expect(resultEntries[2][1]).toBeTruthy();
+  expect((resultEntries[2][1].inputs[0][0] as ConnectionUnit).reactFlowKey).toEqual(concatId);
+*/
+    });
+
     it.skip('creates a looping conditional connection', () => {
       simpleMap['ns0:Root'] = {
         ConditionalLooping: {
