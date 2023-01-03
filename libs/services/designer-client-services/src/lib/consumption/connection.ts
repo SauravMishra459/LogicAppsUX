d08098b78e6052dd229b36cc23464b17bed0875c
Eric <wue@microsoft.com>
2023-01-03 12:21:58 -0500

    consumption work

--- libs/services/designer-client-services/src/lib/consumption/connection.ts
+++ libs/services/designer-client-services/src/lib/consumption/connection.ts
@@ -1,120 +1,150 @@
-// import type {
-//   ConnectionCreationInfo,
-//   ConnectionParametersMetadata,
-//   ConnectorWithSwagger,
-//   CreateConnectionResult,
-//   IConnectionService,
-// } from '../connection';
-// import { UrlService } from '../urlservice';
-// import type { Connection, Connector } from '@microsoft/utils-logic-apps';
-// import { ArgumentException } from '@microsoft/utils-logic-apps';
+import type { IHttpClient } from '../httpClient';
+import type { IOAuthService } from '../oAuth';
+import type { UrlService } from '../urlservice';
+import type { Connection, Connector } from '@microsoft/utils-logic-apps';
+import { addPrefix, clone, extend, getPropertyValue, ArgumentException } from '@microsoft/utils-logic-apps';
 
-// export interface ConsumptionConnectionServiceArgs {
-//   apiVersion: string;
-//   baseUrl?: string;
-//   locale?: string;
-//   apiVersionForSharedManagedConnector: string;
-//   apiVersionForIseManagedConnector: string;
-//   apiVersionForCustomConnector: string;
-//   apiVersionForConnection: string;
-//   apiVersionForGateways: string;
-//   batchApiVersion?: string;
-//   integrationServiceEnvironmentId?: string;
-//   isDynamicInvokeApiEnabled?: boolean;
-//   isIntegrationServiceEnvironmentSupported: boolean;
-//   workflowReferenceId?: string;
-//   location: string;
-//   connectionFailureCallback?(error: any): void;
-//   showViewPermissionsDialog?(connector: Connector): void;
-// }
+export interface ConsumptionConnectionServiceArgs {
+  apiVersion: string;
+  baseUrl?: string;
+  locale?: string;
+  apiVersionForSharedManagedConnector: string;
+  apiVersionForIseManagedConnector: string;
+  apiVersionForCustomConnector: string;
+  apiVersionForConnection: string;
+  apiVersionForGateways: string;
+  batchApiVersion?: string;
+  integrationServiceEnvironmentId?: string;
+  isDynamicInvokeApiEnabled?: boolean;
+  isIntegrationServiceEnvironmentSupported: boolean;
+  workflowReferenceId?: string;
+  location: string;
+  connectionFailureCallback?(error: any): void;
+  showViewPermissionsDialog?(connector: Connector): void;
+  httpClient: IHttpClient;
+  urlService: UrlService;
+  oauthService: IOAuthService;
+}
 
-// export class ConsumptionConnectionService implements IConnectionService {
-//   private _connections: Record<string, Connection> = {};
-//   private _subscriptionResourceGroupWebUrl = '';
-//   private _allConnectionsInitialized = false;
-//   constructor(public readonly options: ConsumptionConnectionServiceArgs) {
-//     const {
-//       baseUrl,
-//       apiVersionForConnection,
-//       apiVersionForIseManagedConnector,
-//       apiVersionForSharedManagedConnector,
-//       apiVersionForCustomConnector,
-//       apiVersionForGateways,
-//     } = options;
-//     if (!baseUrl) {
-//       throw new ArgumentException('baseUrl required');
-//     } else if (!apiVersionForConnection) {
-//       throw new ArgumentException('apiVersionForConnection required');
-//     } else if (!apiVersionForIseManagedConnector) {
-//       throw new ArgumentException('apiVersionForIseManagedConnector required');
-//     } else if (!apiVersionForSharedManagedConnector) {
-//       throw new ArgumentException('apiVersionForSharedManagedConnector required');
-//     } else if (!apiVersionForCustomConnector) {
-//       throw new ArgumentException('apiVersionForCustomConnector required');
-//     } else if (!apiVersionForGateways) {
-//       throw new ArgumentException('apiVersionForGateways required');
-//     }
-//   }
+export class ConsumptionConnectionService {
+  private _options: ConsumptionConnectionServiceArgs;
+  private _client: IHttpClient;
+  private _connections: Record<string, Connection> = {};
+  private _initialized = false;
+  private _oauthService: IOAuthService;
 
-//   dispose(): void {
-//     return;
-//   }
+  constructor(public readonly options: ConsumptionConnectionServiceArgs) {
+    const {
+      baseUrl,
+      urlService,
+      oauthService,
+      apiVersionForConnection,
+      apiVersionForIseManagedConnector,
+      apiVersionForSharedManagedConnector,
+      apiVersionForCustomConnector,
+      apiVersionForGateways,
+    } = options;
+    if (!baseUrl) {
+      throw new ArgumentException('baseUrl required');
+    } else if (!apiVersionForConnection) {
+      throw new ArgumentException('apiVersionForConnection required');
+    } else if (!urlService) {
+      throw new ArgumentException('url service required');
+    } else if (!oauthService) {
+      throw new ArgumentException('oauth service required');
+    } else if (!apiVersionForIseManagedConnector) {
+      throw new ArgumentException('apiVersionForIseManagedConnector required');
+    } else if (!apiVersionForSharedManagedConnector) {
+      throw new ArgumentException('apiVersionForSharedManagedConnector required');
+    } else if (!apiVersionForCustomConnector) {
+      throw new ArgumentException('apiVersionForCustomConnector required');
+    } else if (!apiVersionForGateways) {
+      throw new ArgumentException('apiVersionForGateways required');
+    }
+    this._options = options;
+    this._oauthService = options.oauthService;
+    this._client = options.httpClient;
+  }
 
-//   getConnector(connectorId: string): Promise<Connector> {
-//     throw new Error('Method not implemented.');
-//   }
-//   getConnectorAndSwagger(connectorId: string): Promise<ConnectorWithSwagger> {
-//     throw new Error('Method not implemented.');
-//   }
-//   getSwaggerFromUri(uri: string): Promise<OpenAPIV2.Document> {
-//     throw new Error('Method not implemented.');
-//   }
-//   getConnection(connectionId: string): Promise<Connection> {
-//     throw new Error('Method not implemented.');
-//   }
-//   getConnections(connectorId?: string | undefined): Promise<Connection[]> {
-//     throw new Error('Method not implemented.');
-//   }
-//   createConnection(
-//     connectionId: string,
-//     connector: Connector,
-//     connectionInfo: ConnectionCreationInfo,
-//     parametersMetadata?: ConnectionParametersMetadata | undefined
-//   ): Promise<Connection> {
-//     throw new Error('Method not implemented.');
-//   }
-//   createAndAuthorizeOAuthConnection(
-//     connectionId: string,
-//     connectorId: string,
-//     connectionInfo: ConnectionCreationInfo,
-//     parametersMetadata: ConnectionParametersMetadata
-//   ): Promise<CreateConnectionResult> {
-//     throw new Error('Method not implemented.');
-//   }
-//   getUniqueConnectionName(connectorId: string, connectionNames: string[], connectorName: string): Promise<string> {
-//     throw new Error('Method not implemented.');
-//   }
-//   fetchFunctionApps(): Promise<any> {
-//     throw new Error('Method not implemented.');
-//   }
-//   fetchFunctionAppsFunctions(functionAppId: string): Promise<any> {
-//     throw new Error('Method not implemented.');
-//   }
-//   fetchFunctionKey(functionId: string): Promise<any> {
-//     throw new Error('Method not implemented.');
-//   }
+  init(connections: Connection[]): Record<string, Connection> {
+    if (connections) {
+      connections.forEach((connection) => {
+        const newConnection = this._copyConnection(connection);
+        this._connections[newConnection.id] = newConnection;
+      });
 
-//   init(connections: Connection[]): Connection[] {
-//     if (connections) {
-//       this._initConnections = connections.map((connection) => {
-//         const newConnection = this._copyConnection(connection);
-//         return newConnection;
-//       });
+      this._initialized = true;
+      return this._connections;
+    }
 
-//       this._initialized = true;
-//       return this._initConnections;
-//     }
+    return connections;
+  }
 
-//     return connections;
-//   }
-// }
+  updateConnectionProviders(connectors: Connector[]): Connector[] {
+    let updatedConnectionProviders: Connector[] = [];
+
+    if (connectors) {
+      updatedConnectionProviders = connectors.map((connector) => {
+        return this._getUpdatedConnectionProvider(connector);
+      });
+    }
+
+    return updatedConnectionProviders;
+  }
+
+  showViewPermissionsDialog(connector: Connector): void {
+    if (this._options.showViewPermissionsDialog) {
+      this._options.showViewPermissionsDialog(connector);
+    } else {
+      throw new Error('options.showViewPermissionsDialog required');
+    }
+  }
+
+  private _getUpdatedConnectionProvider(connector: Connector): Connector {
+    let properties = getPropertyValue(connector.properties, 'generalinformation');
+
+    if (properties) {
+      const newConnectionProvider = clone(connector);
+
+      properties = getPropertyValue(newConnectionProvider.properties, 'generalinformation');
+
+      // Pushing the general information as first level properties in api provider properties
+      if (properties.id) {
+        properties.id = addPrefix(properties.id, '/');
+      }
+      newConnectionProvider.properties = extend(newConnectionProvider.properties, properties);
+
+      return newConnectionProvider;
+    }
+
+    return connector;
+  }
+
+  private _copyConnection(connection: Connection): Connection {
+    const properties = connection.properties,
+      parameters = clone(properties.connectionParameters),
+      newConnection: Connection = {
+        name: connection.name,
+        id: connection.id,
+        type: connection.type,
+        properties: {
+          api: properties.api,
+          displayName: properties.displayName,
+          createdTime: properties.createdTime,
+          statuses: properties.statuses,
+          createdBy: properties.createdBy,
+          overallStatus: properties.overallStatus,
+          //   connectionAlternativeParameters: properties.connectionAlternativeParameters,
+          connectionParameters: parameters,
+          connectionParametersSet: properties.connectionParametersSet,
+          testLinks: properties.testLinks,
+        },
+      };
+
+    return newConnection;
+  }
+
+  dispose(): void {
+    return;
+  }
+}
